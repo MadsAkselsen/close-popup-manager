d269c5cd0602ed6561d7d8e544c720fdd13d2064
Mads Akselsen <mads.akselsen@maersk.com>
2022-12-07 12:24:45 +0100

    adding code

---
+++ close-popup-manager.js
@@ -0,0 +1,46 @@
+
+//     element: Element|HTMLElement
+//     ctx: any
+//     closeFn: Function
+
+const elementCloseManager = new class extends EventTarget {
+    constructor() {
+        super();
+        
+        this._closeElements = this._closeElements.bind(this);
+
+        window.addEventListener("click", (e) => {
+            this._closeElements(e);
+        })
+    }
+
+    _elements = [];
+
+    async addElement(closeFn, ctx, element) {
+        // wrapping in setTimeout to make sure the element has been added to the DOM.
+        // Otherwise, getBoundingClientRect() don't know the coordinates of the element.
+        setTimeout(() => {
+            this._elements.push({
+                element,
+                ctx,
+                closeFn
+            });
+            
+        }, 100)
+    }
+
+    _closeElements(e) {
+        const x = e.clientX;
+        const y = e.clientY;
+
+        for (const [i, el] of this._elements.entries()) {
+            const rect = el.element.getBoundingClientRect();
+            const {top, right, bottom, left} = rect;
+
+            if (x < left || x > right || y < top || y > bottom) {
+                el.closeFn.apply(el.ctx);
+                this._elements.splice(i, 1);
+            }
+        }
+    }
+}
---
+++ script.js
@@ -0,0 +1,28 @@
+
+const button1 = document.querySelector("#button-1");
+const button2 = document.querySelector("#button-2");
+const button3 = document.querySelector("#button-3");
+
+const popup1 = document.querySelector("#popup-1");
+const popup2 = document.querySelector("#popup-2");
+const popup3 = document.querySelector("#popup-3");
+
+button1.addEventListener("click", () => togglePopup("popup-1"))
+button2.addEventListener("click", () => togglePopup("popup-2"))
+button3.addEventListener("click", () => togglePopup("popup-3"))
+
+function togglePopup(popupName) {
+    const element = window[popupName];
+    const popupShown = element.getAttribute("shown");
+
+    if (popupShown) {
+        element.removeAttribute("shown");
+    } else {
+        element.setAttribute("shown", true);
+
+        const closePopup = () => {
+            element.removeAttribute("shown");
+        }
+        elementCloseManager.addElement(closePopup, this, element);
+    }
+}
